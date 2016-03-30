2c2715f2737621d6567e5cd214e2e2f69eee7dfa
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-03-30 10:07:47 -0600

    added new tags

--- public/js/main.js
+++ public/js/main.js
@@ -3,50 +3,39 @@ var twitterStream = angular.module('myApp', ['chart.js'])
 twitterStream.controller("mainCtrl", ['$scope', 'socket',
 function ($scope, socket) {
   //chart labels
-  $scope.labels = ["iPhone", "iPad", "Android", "Web Client", "Other"];
+  $scope.labels = ["iPhone", "Android", "Other"];
   //chart colors
-  $scope.colors = ['#6c6a6c','#000000','#7FFD1F','#EC872A', '#9527C2'];
+  $scope.colors = ['#ff0eca','#0038ff', 'rgb(75, 255, 1)'];
   //intial data values
-  $scope.trumpData = [0,0,0,0,0];
-  $scope.sandersData = [0,0,0,0,0];
+  $scope.wcwData = [0,0,0];
+  $scope.mcmData = [0,0,0];
 
   socket.on('newTweet', function (tweet) {
-    $scope.tweet = tweet.text
-    $scope.user = tweet.user.screen_name
+    $scope.tweet = tweet.text;
+    $scope.user = tweet.user.screen_name;
     //parse source from payload
-    var source = tweet.source.split('>')[1].split('<')[0].split(' ')[2]
+    var source = tweet.source.split('>')[1].split('<')[0].split(' ')[2];
     //all hashtags in the tweet
     var hashtags = tweet.entities.hashtags.map(function(el){
-      return el.text.toLowerCase()
-    })
+      return el.text.toLowerCase();
+    });
 
-    //check source and increment for #trump tweets
-    if (hashtags.indexOf('trump') !== -1){
+    //check source and increment for #wcw tweets
+    if (hashtags.indexOf('wcw') !== -1){
       switch (source) {
-        case 'iPhone': $scope.trumpData[0]++
+        case 'iPhone': $scope.wcwData[0]++;
         break;
-        case 'iPad': $scope.trumpData[1]++
+        case 'Android': $scope.wcwData[1]++;
         break;
-        case 'Android': $scope.trumpData[2]++
-        break;
-        case 'Web': $scope.trumpData[3]++
-        break;
-        default: $scope.trumpData[4]++
+        default: $scope.wcwData[2]++;
       }
-    }
-
-    //check source and increment for #feelthebern tweets
-    else if (hashtags.indexOf('feelthebern') !== -1) {
+    } else if (hashtags.indexOf('mcm') !== -1){
       switch (source) {
-        case 'iPhone': $scope.sandersData[0]++
+        case 'iPhone': $scope.mcmData[0]++;
         break;
-        case 'iPad': $scope.sandersData[1]++
+        case 'Android': $scope.mcmData[1]++;
         break;
-        case 'Android': $scope.sandersData[2]++
-        break;
-        case 'Web': $scope.sandersData[3]++
-        break;
-        default: $scope.sandersData[4]++
+        default: $scope.mcmData[2]++;
       }
     }
   });
